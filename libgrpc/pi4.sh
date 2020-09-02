176bb6f489c0167dc49a559ab4f8d0b4f8ac1586
Wayne Taylor <wayne@kryptos.co.uk>
2020-09-02 22:26:51 +0100

    Cleaning up the libgrpc folder

--- libgrpc/pi4.sh
+++
@@ -1,6 +0,0 @@
-cp /home/pi/chatbot/libgrpc/pi4/libgrpc_csharp_ext.x86.so /home/pi/chatbot/Bot/bin/Debug/netcoreapp3.1/runtimes/linux/native/
-cp /home/pi/chatbot/libgrpc/pi4/libgrpc_csharp_ext.x86.so /home/pi/chatbot/Bot/bin/Debug/netcoreapp3.1/runtimes/linux-arm/native/
-cp /home/pi/chatbot/libgrpc/pi4/libgrpc_csharp_ext.x86.so /home/pi/chatbot/Bot/bin/Debug/netcoreapp3.1/runtimes/win/native/
-
-cd ~/chatbot/Bot
-dotnet run --no-build
