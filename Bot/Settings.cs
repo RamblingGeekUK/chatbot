25e63a978a305a3dab3d69a4de7da7e4ab312578
Wayne Taylor <wayne@kryptos.co.uk>
2020-09-02 17:21:38 +0100

    Merging vNext with Master

--- Bot/Settings.cs
+++ Bot/Settings.cs
@@ -4,12 +4,7 @@ namespace ChatBot
 {
     public class Settings
     {
-        public string Twitch_channel;
-
-
-        Twitch_channel = "Hello World"; //Environment.GetEnvironmentVariable("Twitch_channel");
-
-
+        public static string Twitch_channel = Environment.GetEnvironmentVariable("Twitch_channel");
         public static string Twitch_botusername = Environment.GetEnvironmentVariable("Twitch_botusername");
         public static string Twitch_token = Environment.GetEnvironmentVariable("Twitch_token");
         public static string Twitch_ID = Environment.GetEnvironmentVariable("Twitch_ID");
