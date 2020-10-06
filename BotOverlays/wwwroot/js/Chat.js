b5e1e0c066ff07486c64f2ba617f03267b39f55e
Wayne Taylor <wayne@kryptos.co.uk>
2020-10-06 17:40:19 +0100

    Updated Readme - Working on chat overlay

--- BotOverlays/wwwroot/js/Chat.js
+++ BotOverlays/wwwroot/js/Chat.js
@@ -1,24 +1,37 @@
 ﻿"use strict";
 
 var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
+var toastCounter = 0;
 
 connection.on("ReceiveTwitchMessage", function (user, message) {
+    CreateToast(user, message);
+    //var thisToast = toastCounter - 1;
+
+    //// Make it slide down
+    //$(document).find("#chatmsg_" + thisToast).slideDown(600);
+
+    //setTimeout(function () {
+    //    $(document).find("#chatmsg_" + thisToast).slideUp(600, function () {   // Slideup callback executes AFTER the slideup effect.
+    //        $(this).remove();
+    //    });
+    //}, 3000);  // 3sec.
+});
+
+function CreateToast(user, message) {
     // Main
     var main = document.createElement('div');
-    main.className = 'toast fade show';
-    main.id = "chatmsg";
-    main.setAttribute('data-autohide', true);
+    main.className = 'toast';
+    main.id = "chatmsg_" + toastCounter;
+    main.setAttribute('role', 'alert');
+    main.setAttribute('aria-live', 'assertive');
+    main.setAttribute('aria-atomic', 'true');
+    main.setAttribute('data-autohide', 'true');
 
     // Header
     var header = document.createElement('div');
     header.className = 'toast-header';
     header.innerHTML = user;
     main.appendChild(header);
-
-    //// Add Close Button to Header
-    //var closebut = document.createElement('button');
-    //closebut.className = 'm1-2 mb-1 close';
-    //closebut.appendChild(header);
 
     // Body
     var body = document.createElement('div');
@@ -27,16 +40,19 @@ connection.on("ReceiveTwitchMessage", function (user, message) {
     main.appendChild(body);
 
     // Append to the DOM
-    document.body.appendChild(main);
+    //document.body.appendChild(main);
+    document.getElementById("twitchchat").appendChild(main);
+
+    // Increment toastCounter
+    toastCounter++;  
 
     // Show Toast
-    $("#twitchchat").toast('show');
-
-
-});
+    //$("#twitchchat" + toastCounter).toast('show');
+  
+}
 
 connection.start().then(function () {
-    $("#test").toast('show');
+    
 }).catch(function (err) {
     return console.error(err.toString());
 });
